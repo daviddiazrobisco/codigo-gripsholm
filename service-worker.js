7a507b9ddd478259c3a89a091149fdf6e269830f
daviddiazrobisco <david.diaz.robisco@gmail.com>
2026-08-26 00:21:38 +0200

    Cierra Día 9 con viñetas y contexto histórico

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="gripsholm-v8-days00-10-v89";
+const CACHE="gripsholm-v8-days00-10-v93";
 const FILES=[
   "./","./index.html","./dia0.html","./dia1.html","./dia2.html","./dia3.html","./dia4.html","./dia5.html","./dia6.html","./dia7.html","./dia8.html","./dia9.html","./dia10.html","./styles.css","./connection-status.js","./app.js","./app-dia1.js","./app-dia2.js","./app-dia5.js","./app-dia6.js","./day0-v7-data.js","./day1-v7-data.js","./day2-v7-data.js","./day3-v7-data.js","./day4-v7-data.js","./day5-v7-data.js","./day6-v7-data.js","./day7-v7-data.js","./day8-v7-data.js","./day9-v7-data.js","./day10-v7-data.js","./manifest.webmanifest","./assets/icons/icon-192.png","./assets/icons/icon-512.png","./assets/icons/icon.svg",
   "./assets/data/nordic-map-data.js","./assets/data/world-map-data.js",
@@ -86,6 +86,22 @@ const FILES=[
   ,"./assets/images/d9-tira-corona-comic-v1.png"
   ,"./assets/images/d9-tira-llaves-comic-v1.png"
   ,"./assets/images/d9-leon-casco-comparativa-ilustracion-v1.png"
+  ,"./assets/images/d9-kalmar-castle-cc-by-sa.jpg"
+  ,"./assets/images/d9-stockholm-royal-palace-cc-by-sa.jpg"
+  ,"./assets/images/d9-karlsborg-fortress-cc-by.jpg"
+  ,"./assets/images/d9-gripsholm-castle-cc-by-sa.jpg"
+  ,"./assets/images/d9-pax-vertical-01-corona-v1.png"
+  ,"./assets/images/d9-pax-vertical-02-intercambio-v1.png"
+  ,"./assets/images/d9-pax-vertical-03-monasterio-v1.png"
+  ,"./assets/images/d9-pax-vertical-04-recupera-v1.png"
+  ,"./assets/images/d9-pax-vertical-05-castillo-v1.png"
+  ,"./assets/images/d9-corona-vertical-01-olaf-v1.png"
+  ,"./assets/images/d9-corona-vertical-02-erik-v1.png"
+  ,"./assets/images/d9-corona-vertical-03-rivalidades-v1.png"
+  ,"./assets/images/d9-corona-vertical-04-sture-v1.png"
+  ,"./assets/images/d9-corona-vertical-05-cristian-v1.png"
+  ,"./assets/images/d9-corona-vertical-06-gustavo-v1.png"
+  ,"./assets/images/d9-corona-vertical-00-bo-grip-v1.png"
 ];
 const OFFLINE_MEDIA=[
   "./assets/data/ne_10m_admin_0_countries.geojson","./assets/data/ne_10m_lakes.geojson","./assets/data/ne_110m_admin_0_countries.geojson",
